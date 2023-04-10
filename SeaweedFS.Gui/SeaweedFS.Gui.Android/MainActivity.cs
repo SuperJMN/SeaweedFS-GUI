01e459b8e9cbeba8bb6c75a96526f4f42f37195c
José Manuel Nieto <superjmn@outlook.com>
2023-04-10 23:22:14 +0200

    Migrate to Avalonia 11 Preview 6

--- SeaweedFS.Gui/SeaweedFS.Gui.Android/MainActivity.cs
+++ SeaweedFS.Gui/SeaweedFS.Gui.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Avalonia.Android;
 namespace SeaweedFS.Gui.Android;
 
 [Activity(Label = "SeaweedFS.Gui.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon",
-    LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity
 {
 }
